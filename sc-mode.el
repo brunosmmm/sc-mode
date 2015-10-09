1fa41edc6e4ed3c7a7b2b53397a94732f7a1cd48
brunosmmm <brunosmmm@gmail.com>
2015-10-09 13:47:51 -0400

    Getting rid of annoying elisp syntax warnings

--- sc-mode.el
+++ sc-mode.el
@@ -6,7 +6,7 @@
 (require 'font-lock)
 (require 'cc-mode)
 
-(defun check-and-load ()
+(defun check-and-load () "Check if this is a SpecC file by looking at the file name and load sc-mode automatically."
 
   ;;check that this is a SpecC file and if so load sc-mode
   (when (string-match ".*\\.\\(sc\\|sh\\)$" buffer-file-name)
@@ -22,8 +22,8 @@
   (if (string-equal major-mode "c-mode")
       (progn
         ;; define our keywords
-        (defvar thekwords)
-        (setq thekwords
+        (defvar sc-mode-kwords)
+        (setq sc-mode-kwords
               '(("\\_<\\(note\\|seq\\|import\\|try\\|interrupt\\|trap\\|par\\|fsm\\|pipe\\|notify\\|range\\|timing\\|wait\\(for\\)?\\)\\_>" . font-lock-keyword-face)
                 ;; maybe it would be good to move some of those to builtin face to get extra emphasis
                 ("\\_<\\(signal\\|buffered\\|piped\\|interface\\|behavior\\|channel\\|event\\)\\_>" . font-lock-type-face)
@@ -36,8 +36,8 @@
                 ))
 
         (if sc-mode
-            (font-lock-add-keywords nil thekwords)
-          (font-lock-remove-keywords nil thekwords)
+            (font-lock-add-keywords nil sc-mode-kwords)
+          (font-lock-remove-keywords nil sc-mode-kwords)
           )
 
         ;; update buffer
