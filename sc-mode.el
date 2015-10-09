4346663f63c44b4bfd4b8b9f093140432754c59e
brunosmmm <brunosmmm@gmail.com>
2015-10-09 11:51:07 -0400

    Guard to prevent changes when not in c-mode

--- sc-mode.el
+++ sc-mode.el
@@ -17,31 +17,36 @@
   "SpecC keywords and other on top of cc-mode"
   :lighter " SpecC"
 
-  ;; define our keywords
-  (defvar thekwords)
-  (setq thekwords
-        '(("\\_<\\(note\\|seq\\|import\\|try\\|interrupt\\|trap\\|par\\|fsm\\|pipe\\|notify\\|implements\\|range\\|timing\\|wait\\(for\\)?\\)\\_>" . font-lock-keyword-face)
-          ;; maybe it would be good to move some of those to builtin face to get extra emphasis
-          ("\\_<\\(signal\\|buffered\\|piped\\|interface\\|behavior\\|channel\\|event\\)\\_>" . font-lock-type-face)
-          ;; bit type
-          ("\\_<\\(bit\\)\\(\[[0-9+]:[0-9]+\]\\)?\\_>" 1 font-lock-type-face)
-          ;;input / output declarations
-          ("\\_<\\(in\\)?\\(out\\)?\\_>" . font-lock-builtin-face)
-          ))
+  ;; check if in C mode
 
-  (if sc-mode
-      (font-lock-add-keywords nil thekwords)
-    (font-lock-remove-keywords nil thekwords)
-    )
+  (if (string-equal major-mode "c-mode")
+      (progn
+        ;; define our keywords
+        (defvar thekwords)
+        (setq thekwords
+              '(("\\_<\\(note\\|seq\\|import\\|try\\|interrupt\\|trap\\|par\\|fsm\\|pipe\\|notify\\|range\\|timing\\|wait\\(for\\)?\\)\\_>" . font-lock-keyword-face)
+                ;; maybe it would be good to move some of those to builtin face to get extra emphasis
+                ("\\_<\\(signal\\|buffered\\|piped\\|interface\\|behavior\\|channel\\|event\\)\\_>" . font-lock-type-face)
+                ;; implements keyword (doesn't work)
+                ("\\_<implements\\_>" 1 font-lock-keyword-face)
+                ;; bit type
+                ("\\_<\\(bit\\)\\(\[[0-9+]:[0-9]+\]\\)?\\_>" 1 font-lock-type-face)
+                ;;input / output declarations
+                ("\\_<\\(in\\)?\\(out\\)?\\_>" . font-lock-builtin-face)
+                ))
 
-  ;; update buffer
-  (if (fboundp 'font-lock-flush)
-      (font-lock-flush)
-    (when font-lock-mode
-      (with-no-warnings
-        (font-lock-fontify-buffer))))
+        (if sc-mode
+            (font-lock-add-keywords nil thekwords)
+          (font-lock-remove-keywords nil thekwords)
+          )
 
-  )
+        ;; update buffer
+        (if (fboundp 'font-lock-flush)
+            (font-lock-flush)
+          (when font-lock-mode
+            (with-no-warnings
+              (font-lock-fontify-buffer)))))
+    (message "not in c-mode")))
 
 ;; add C mode hook
 ;;;###autoload
